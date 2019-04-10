12472219627eba619d65c64dfb5023f37d8f8e13
A00432835 <46534961+A00432835@users.noreply.github.com>
2019-04-09 22:13:57 -0300

    Changes in Views and Payment Validations

--- Tasty/Scripts/validation.js
+++ Tasty/Scripts/validation.js
@@ -1,13 +1,4 @@
-﻿
-
-
-
-
-
-
-
-
-function getElems() {
+﻿function getElems() {
     var NameOnCard = document.getElementById("nameOnCard").value;
     var cardNumber = document.getElementById("cardNumber").value;
     var expiryDate = document.getElementById("expiryDate").value;
@@ -19,7 +10,14 @@ function getElems() {
         return false;
     }
     else {
-        checkValues(NameOnCard, cardNumber, expiryDate, cvv);
+        var value = checkValues(NameOnCard, cardNumber, expiryDate, cvv);
+        if (!value) {
+            document.getElementById('error').innerHTML = "Values are wrong";
+            document.getElementById('error').style.color = "red";
+            return false;
+        } else {
+            return true;
+        }
     }
 }
 
@@ -27,26 +25,29 @@ function getElems() {
 function checkValues(NameOnCard, cardNumber, expiryDate, cvv) {
     var regexAlpha = "[a-zA-Z]+";
     var regexNumber = "[0-9]+";	
+    var regexExp = "(0[1-9]|10|11|12)\/(1[6-9]|2[0-9]|30|31)";
+    stringCvv = cvv;
+    
     //var firstDig = cardNumber.substr(0,1)
     //var twoDig = cardNumber.substr(0, 2)
     if (!NameOnCard.match(regexAlpha)) {
         return false;
     }
 
-    if (cardNumber.length > 13 && cardNumber.length <= 16) {
+    if (cardNumber.length > 14 && cardNumber.length <= 16) {
         if (!cardNumber.match(regexNumber)) {
             return false;
         }
     }
-    if ((!expiryDate.match(regexNumber)) && expiryDate.length != 4) {
+    if ((expiryDate.length != 5 || !(expiryDate.match(regexExp)))) {
         return false;
     }
-    else {
-        var month = expiryDate.substr(0, 2)
-        if (!(month > 0) && !(month <= 12)) {
-            return false
-        }
+    if (!(cvv.match(regexNumber)) || stringCvv.length !=3) {
+        return false;
+    }
+
+    return true;
     }
     //else if ()
 
-}
+
